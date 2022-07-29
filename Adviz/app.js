8f918e01e85c9bb173ffc3f84f4844a100806e80
Thien Duong <thien.duong@finlex.de>
2022-07-29 17:36:32 +0200

    minor change on routers

--- Adviz/app.js
+++ Adviz/app.js
@@ -1,15 +1,16 @@
 //add dependencies
 const express = require('express');
-
+const indexRouter = require('./routes/index');
+const path = require('path');
+const morgan = require('morgan');
 //Variable init
-const app = express()
-const port = 3000
-// app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
+const port = 3000;
+app.use(morgan('dev'));
+app.use('/', indexRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
+
+module.exports = app;
